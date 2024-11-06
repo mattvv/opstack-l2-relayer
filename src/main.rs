424777bda5a859afa41ece05fb762c1b18a88037
Tyler Smith <mail@tcry.pt>
2024-11-06 12:49:23 +0700

    Add Identifier and SentMessage types.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,85 @@
 use alloy::{
-    primitives::address,
+    primitives::{address, Address, U256},
     providers::{Provider, ProviderBuilder, WsConnect},
     rpc::types::{BlockNumberOrTag, Filter},
 };
+
+use alloy_rpc_types_eth::Log;
 use eyre::Result;
 use futures_util::stream::StreamExt;
+
+struct Identifier {
+    origin: Address,
+    block_number: U256,
+    log_index: U256,
+    timestamp: U256,
+    chain_id: U256,
+}
+
+struct SentMessage {
+    destination: U256,
+    target: Address,
+    message_nonce: U256,
+    sender: Address,
+    message: Vec<u8>,
+    block_number: U256,
+    timestamp: U256,
+
+    chain_id: U256,
+}
+
+impl SentMessage {
+    fn from_log(chain_id: U256, log: Log) -> Self {
+        let block_number = U256::from(log.block_number.unwrap());
+        let timestamp = U256::from(log.block_timestamp.unwrap());
+
+        let topics = log.topics();
+        if  topics.len() != 4 {
+            panic!("Invalid number of topics");
+        }
+
+        let log_data = log.data().clone();
+        log_data.data.len();
+        if log_data.data.len() < 20 {
+            panic!("Invalid data length");
+        }
+
+        let _signature = topics[0];
+
+        let dest_bytes = topics[1].as_slice();
+        let destination = U256::from_be_slice(dest_bytes);
+
+        let target = Address::from_word(topics[2].into());
+        let message_nonce: U256 = topics[3].into();
+
+        let sender_bytes = log_data.data[0..20].to_vec();
+        let sender = Address::from_slice(&sender_bytes);
+
+        let message = log_data.data[20..].to_vec();
+
+
+        Self {
+            destination,
+            target,
+            message_nonce,
+            sender,
+            message,
+            block_number,
+            timestamp,
+            chain_id
+        }
+    }
+
+    fn id(&self) -> Identifier {
+        Identifier {
+            origin: self.sender,
+            block_number: self.block_number,
+            log_index: self.message_nonce,
+            timestamp: self.timestamp,
+            chain_id: self.chain_id,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
